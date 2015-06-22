ad11a725bef13ac94e8ca3f97141677f075679ea
ORest <orestprustayko@gmail.com>
2015-06-22 23:41:52 +0300

    working on classes

--- src/js/classes.js
+++ src/js/classes.js
@@ -1,4 +1,3 @@
-//http://ilikekillnerds.com/2015/02/a-guide-to-es6-classes/
 //http://www.2ality.com/2015/02/es6-classes-final.html
 
 //Classes are syntactical sugar over existing prototyped-based inheritance. The class syntax doesn't introduce new
@@ -12,19 +11,46 @@ class Polygon {
         this.height = height;
         this.width = width;
     }
+
+    getSquare() {
+        return this.height * this.width;
+    }
 };
 
 //need to remember that class declarations are not hoisted
-//var p = new Polygon(); reference error
+//var p = new Polygon(); reference error //hoisting is not working:(
 //class Polygon {...}
 
 //Class expression
 
-var Polygon = class {
+/*var Polygon = class {
 //or named
 //var Polygon = Polygon class {
-    constructor(height, width) {
+    constructor(height, width) { //there can be only one "constructor" method
         this.height = height;
         this.width = width;
     }
-};
+};*/
+
+//class inheritance
+
+class Area extends Polygon {
+    constructor (height, width) {
+        super(height, width);
+    }
+
+    //overriding
+    getSquare() {
+        var square = super.getSquare();
+        
+        return square * 2;
+    }
+
+    doSomething() {
+        console.log('do something');
+    }
+}
+
+var area = new Area(10,5);
+
+console.log(area);
